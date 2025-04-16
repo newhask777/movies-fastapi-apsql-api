b8007166d48d8980c06ab4c90b1e2c18a6f7c7b2
newhask777 <newhaskellisp@gmail.com>
2025-04-16 20:16:36 +0300

    last

--- app/migrations/env.py
+++ app/migrations/env.py
@@ -9,14 +9,19 @@ from alembic import context
 
 sys.path.insert(0, dirname(dirname(dirname(abspath(__file__)))))
 
-from app.database import DATABASE_URL, Base
-from app.hotels.models import Hotels
+
+from app.database import Base
+from app.hotels.models import Hotels, Rooms
+from app.bookings.models import Bookings
+from app.users.models import Users
+
+from app.config import settings
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
 
-config.set_main_option("sqlalchemy.url", f"{DATABASE_URL}?async_fallback=True")
+config.set_main_option("sqlalchemy.url", f"{settings.DATABASE_URL}?async_fallback=True")
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
